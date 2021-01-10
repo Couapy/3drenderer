e85700a94bf10f67e01c41b393450c24b7a34d73
Couapy <mael@marchand.cloud>
2021-01-10 23:06:18 +0100

    feat: add rotation to objects

--- src/src/main/java/cloud/marchand/renderer/models/Object3D.java
+++ src/src/main/java/cloud/marchand/renderer/models/Object3D.java
@@ -1,6 +1,7 @@
 package cloud.marchand.renderer.models;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import cloud.marchand.renderer.interfaces.Observable;
@@ -9,6 +10,7 @@ public class Object3D extends Observable {
 
     protected List<Point3D> nodes = new ArrayList<>();
     protected List<Link3D> links = new ArrayList<>();
+    protected Point3D centralPoint;
     private double rotationX = 0.0;
     private double rotationY = 0.0;
     private double rotationZ = 0.0;
@@ -21,10 +23,22 @@ public class Object3D extends Observable {
         this.links = links;
     }
 
+    public List<Point3D> getNodes() {
+        return nodes;
+    }
+
+    public List<Link3D> getLinks() {
+        return links;
+    }
+
+    public Point3D getCentralPoint() {
+        return centralPoint;
+    }
+
     public double getRotationX() {
         return rotationX;
     }
-    
+
     public double getRotationY() {
         return rotationY;
     }
@@ -32,7 +46,7 @@ public class Object3D extends Observable {
     public double getRotationZ() {
         return rotationZ;
     }
-    
+
     public synchronized void rotateX(double rotationX) {
         this.rotationX += rotationX;
         while (this.rotationX < 0) {
@@ -42,7 +56,13 @@ public class Object3D extends Observable {
             this.rotationX -= 180;
         }
 
-        // TODO: rotation around the axe
+        Iterator<Point3D> iterator = nodes.iterator();
+        double cy = centralPoint.getY(), cz = centralPoint.getZ();
+        while (iterator.hasNext()) {
+            Point3D point = iterator.next();
+            point.setY((point.getY() - cy) * Math.cos(rotationX) + cy);
+            point.setZ((point.getZ() - cz) * Math.cos(rotationX) + cz);
+        }
     }
 
     public synchronized void rotateY(double rotationY) {
@@ -54,7 +74,13 @@ public class Object3D extends Observable {
             this.rotationY -= 180;
         }
 
-        // TODO: rotation around the axe
+        Iterator<Point3D> iterator = nodes.iterator();
+        double cx = centralPoint.getX(), cz = centralPoint.getZ();
+        while (iterator.hasNext()) {
+            Point3D point = iterator.next();
+            point.setX((point.getX() - cx) * Math.cos(rotationY) + cx);
+            point.setZ((point.getZ() - cz) * Math.cos(rotationY) + cz);
+        }
     }
 
     public synchronized void rotateZ(double rotationZ) {
@@ -66,7 +92,13 @@ public class Object3D extends Observable {
             this.rotationZ -= 180;
         }
 
-        // TODO: rotation around the axe
+        Iterator<Point3D> iterator = nodes.iterator();
+        double cx = centralPoint.getX(), cy = centralPoint.getY();
+        while (iterator.hasNext()) {
+            Point3D point = iterator.next();
+            point.setX((point.getX() - cx) * Math.cos(rotationZ) + cx);
+            point.setY((point.getY() - cy) * Math.cos(rotationZ) + cy);
+        }
     }
 
 }
---
+++ src/src/main/java/cloud/marchand/renderer/models/geometricShapes/Parallelepiped.java
@@ -0,0 +1,46 @@
+package cloud.marchand.renderer.models.geometricShapes;
+
+import cloud.marchand.renderer.models.Link3D;
+import cloud.marchand.renderer.models.Object3D;
+import cloud.marchand.renderer.models.Point3D;
+
+public class Parallelepiped extends Object3D {
+
+    public Parallelepiped() {
+        this(100, 200, 300);
+    }
+
+    public Parallelepiped(double width, double deep, double height) {
+        // Create nodes of cube
+        // Bottom square
+        nodes.add(new Point3D(0,     0,    0));
+        nodes.add(new Point3D(width, 0,    0));
+        nodes.add(new Point3D(0,     deep, 0));
+        nodes.add(new Point3D(width, deep, 0));
+        // Top square
+        nodes.add(new Point3D(0,     0,    height));
+        nodes.add(new Point3D(width, 0,    height));
+        nodes.add(new Point3D(0,     deep, height));
+        nodes.add(new Point3D(width, deep, height));
+
+        // Bottom square
+        links.add(new Link3D(nodes.get(0), nodes.get(1)));
+        links.add(new Link3D(nodes.get(1), nodes.get(2)));
+        links.add(new Link3D(nodes.get(2), nodes.get(3)));
+        links.add(new Link3D(nodes.get(3), nodes.get(0)));
+        // Top square
+        links.add(new Link3D(nodes.get(4), nodes.get(5)));
+        links.add(new Link3D(nodes.get(5), nodes.get(6)));
+        links.add(new Link3D(nodes.get(6), nodes.get(7)));
+        links.add(new Link3D(nodes.get(7), nodes.get(4)));
+        // Between squares
+        links.add(new Link3D(nodes.get(0), nodes.get(4)));
+        links.add(new Link3D(nodes.get(1), nodes.get(5)));
+        links.add(new Link3D(nodes.get(2), nodes.get(6)));
+        links.add(new Link3D(nodes.get(3), nodes.get(7)));
+        
+        // Central point of the object
+        centralPoint = new Point3D(width / 2, deep / 2, height / 2);
+    }
+    
+}
